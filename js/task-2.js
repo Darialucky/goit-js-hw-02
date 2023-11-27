2c298e67db6322f5199b6d67627954845d0d0c97
Darialucky <144655337+Darialucky@users.noreply.github.com>
2023-11-27 21:03:46 +0200

    3

--- js/task-2.js
+++ js/task-2.js
@@ -13,7 +13,7 @@ function formatMessage(message, maxLength) {
   if (message.length <= maxLength) {
     return message;
   }
-  else {return message.substring(0, maxLength) + '...';
+  else {return message.slice(0, maxLength) + '...';
 
   }
 }
