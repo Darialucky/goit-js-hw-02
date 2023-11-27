ad8419569c7969eaf600a37870a77cfbdf34d515
Darialucky <144655337+Darialucky@users.noreply.github.com>
2023-11-27 19:08:31 +0200

    2

--- js/task-3.js
+++ js/task-3.js
@@ -20,11 +20,10 @@ function checkForSpam(message) {
         return true; 
       }
     }
-  
     return false; 
   }
 
-  console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
 console.log(checkForSpam("Amazing SalE, only tonight!")); // true
